89c83a3d57ff6aec26c22acabb996ed15ffbf03d
Alejandro Fernandez Restrepo <afernander@eafit.edu.co>
2021-10-05 18:36:13 -0500

    final version
    fixed varius things

--- SleepingBarber.c
+++ SleepingBarber.c
@@ -13,7 +13,6 @@
 void *customer(void *num);
 void *barber(void *);
 
-void randwait(int secs);
 void cuttime(int num);
 void timearrive(int time);
 
@@ -27,13 +26,13 @@ sem_t waitingRoom;
 // the barber chair.
 sem_t barberChair;
 
-// barberPillow is used to allow the barber to sleep
+// barberSleep is used to allow the barber to sleep
 // until a customer arrives.
-sem_t barberPillow;
+sem_t barberSleep;
 
-// seatBelt is used to make the customer to wait until
+// hairCut is used to make the customer to wait until
 // the barber is done cutting his/her hair.
-sem_t seatBelt;
+sem_t hairCut;
 
 // Stop program when all customers leave the barber
 int allDone = 0;
@@ -62,8 +61,8 @@ int main(int argc, char *argv[]) {
   // Initialize the semaphores
   sem_init(&waitingRoom, 0, numChairs);
   sem_init(&barberChair, 0, 1);
-  sem_init(&barberPillow, 0, 0);
-  sem_init(&seatBelt, 0, 0);
+  sem_init(&barberSleep, 0, 0);
+  sem_init(&hairCut, 0, 0);
 
   // Create the barber.
   pthread_create(&btid, NULL, barber, NULL);
@@ -80,10 +79,9 @@ int main(int argc, char *argv[]) {
   sleep(1);
   }
 
-  // When all of the customers are finished, kill the
-  // barber thread.
+  // When all of the customers are finished, finish the barber
   allDone = 1;
-  sem_post(&barberPillow); // Wake the barber so he will exit.
+  sem_post(&barberSleep); // Wake the barber so he will exit.
   pthread_join(btid,NULL);
   return 0;
 }
@@ -95,7 +93,7 @@ void *customer(void *number) {
   // Leave for the shop and take some random amount of
   printf("Customer %d is going to the barber shop.\n", num);
   // random time to arrive (0 - 3 seg).
-  timearrive(rand() %3);
+  sleep(rand()%3);
   printf("Customer %d arrived at barber shop.\n", num);
 
   // Wait for space to open up in the waiting room
@@ -115,14 +113,14 @@ void *customer(void *number) {
   sem_post(&waitingRoom);
   
   // Wake up the barber if it is sleeping
-  sem_getvalue(&barberPillow,&wake);
+  sem_getvalue(&barberSleep,&wake);
   if(wake==0){
       printf("Customer %d waking the barber.\n", num);
-  sem_post(&barberPillow);
+  sem_post(&barberSleep);
   }
 
   // Wait for the barber to finish cutting your hair.
-  sem_wait(&seatBelt);
+  sem_wait(&hairCut);
 
   // Give up the chair.
   sem_post(&barberChair);
@@ -138,7 +136,7 @@ void *barber(void *junk) {
     sem_getvalue(&waitingRoom,&customers);
     if(customers==10){
       printf("The barber is sleeping \n");
-      sem_wait(&barberPillow);
+      sem_wait(&barberSleep);
     }
     
     if (!allDone) {
@@ -148,7 +146,7 @@ void *barber(void *junk) {
     cuttime(rand() %4);
     printf("The barber has finished cutting hair.\n");
     // Release the customer when done cutting
-    sem_post(&seatBelt);
+    sem_post(&hairCut);
     }
     else {
     printf("The barber is going home for the day.\n");
@@ -156,18 +154,6 @@ void *barber(void *junk) {
   }
 }
 
-void randwait(int secs) {
-  int len;
-
-  // Generate a random number…
-  len = (int) ((1 * secs) + 1);
-  sleep(len);
-}
-
-//time for a customer to arrive
-void timearrive(int time){
-  sleep(time);
-}
 //cutting hair time for each type there is a diferent waitng time
 void cuttime(int num){
   printf("The customer want the haircut number %d\n",num);
